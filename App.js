023579f3c103013e02d0e0b49dd1a2cb695eddc0
Wilson <121342723+WilsonKumpa@users.noreply.github.com>
2023-03-21 17:26:03 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import SimpleStorage from "./SimpleStorage";
+import ContractToggle from "./toggle";
+
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleStorage />
+      <ContractToggle />
+    </div>
+  );
+}
+
+export default App;
